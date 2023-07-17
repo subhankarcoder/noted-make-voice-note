04fb70dd12f4b33601bd0d60193d391c62c1528b
subhankarcoder <subhankarchakrabarti0@gmail.com>
2023-07-17 22:29:00 +0530

    Made some changes

--- src/Components/Noted.js
+++ src/Components/Noted.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
@@ -9,16 +9,50 @@ const Noted = () => {
   const [inputData, setInputData] = useState("");
   const [items, setItems] = useState([]);
   const [textToCopy, setTextToCopy] = useState();
-  const addItem = () => {
-    if (!inputData) {
-    } else {
-      setItems([...items, inputData]);
-      setInputData("");
-    }
-  };
+  const [points, setPoints] = useState(0);
+  // const [showGif, setShowGif] = useState(false);
   const [isCopied, setCopied] = useClipboard(textToCopy, {
     successDuration: 1000,
   });
+  const [priority, setPriority] = useState("low");
+  useEffect(() => {
+    // Restore items from local storage on component mount
+    const storedItems = localStorage.getItem("notedItems");
+    if (storedItems) {
+      setItems(JSON.parse(storedItems));
+    }
+  }, []);
+//   if(window.localStorage) {
+//     alert('ls exists');
+// } else {
+//     alert('ls does not exist');
+// }
+
+  useEffect(() => {
+    // Save items to local storage whenever items state changes
+    localStorage.setItem("notedItems", JSON.stringify(items));
+  }, [items]);
+  const addItem = () => {
+    if (!inputData) {
+      return;
+    }
+    const newItem = {
+      text: inputData,
+      priority: priority,
+    };
+    setItems([...items, newItem]);
+    setInputData("");
+  };
+  const deleteItem = (id) => {
+    console.log(id);
+    const updatedItems = items.filter((elem, ind) => {
+      return ind != id;
+    });
+    setPoints(points + 10);
+    setItems(updatedItems);
+    const succsess = new Audio("./images/success_sound.mp3");
+    succsess.play();
+  };
   const startListening = () =>
     SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
   const { transcript, browserSupportsSpeechRecognition, resetTranscript } =
@@ -27,8 +61,15 @@ const Noted = () => {
   if (!browserSupportsSpeechRecognition) {
     return null;
   }
+  const handlePriorityClick = (selectedPriority) => {
+    setPriority(selectedPriority);
+  };
   return (
     <>
+      <section className="points">
+        <img src="./images/token.png" />
+        <p>Points : {points}</p>
+      </section>
       <section className="noted-input">
         <div className="message-input">
           <h1>Speak your message below</h1>
@@ -63,9 +104,24 @@ const Noted = () => {
         <div className="priority">
           <p>What is the priority of your task?</p>
           <div className="priority-btn-holder">
-            <button className="priority-btn high">High</button>
-            <button className="priority-btn medium">Medium</button>
-            <button className="priority-btn low">Low</button>
+            <button
+              className="priority-btn high"
+              onClick={() => handlePriorityClick("high")}
+            >
+              High
+            </button>
+            <button
+              className="priority-btn medium"
+              onClick={() => handlePriorityClick("medium")}
+            >
+              Medium
+            </button>
+            <button
+              className="priority-btn low"
+              onClick={() => handlePriorityClick("low")}
+            >
+              Low
+            </button>
           </div>
         </div>
         <div className="add-holder">
@@ -76,11 +132,19 @@ const Noted = () => {
       </section>
       <section className="noted-output">
         {items.map((elem, index) => {
+          const { text, priority } = elem;
           return (
-            <div className="output-card">
-              <div className="span"></div>
-              <p>{elem}</p>
-            </div>
+            <>
+              <div key={index} className="output-card">
+                <button className="complete" onClick={() => deleteItem(index)}>
+                  Completed
+                </button>
+                <div className="overlay">
+                  <p>{text}</p>
+                  <div className={`span ${priority}`}></div>
+                </div>
+              </div>
+            </>
           );
         })}
       </section>
